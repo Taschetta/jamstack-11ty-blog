db315a13344ef818c70208eafab64613d730bb33
santiago <santiagotaschetta@gmail.com>
2021-08-13 19:41:04 -0300

    fixed admin panel

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
 const { DateTime } = require('luxon')
 
 module.exports = (eleventyConfig) => {
+
   eleventyConfig.addPassthroughCopy('./src/style.css')  
   eleventyConfig.addPassthroughCopy('./src/assets')  
+  eleventyConfig.addPassthroughCopy('./src/admin')  
 
   eleventyConfig.addFilter('postDate', (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
